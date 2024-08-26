10f95e51b95f61b6870216b054c9a70e07e8abb9
Abhi <abhishek.pagar839@gmail.com>
2024-08-26 16:03:59 +0530

    Sum Of Two Numbers

---
+++ Sumoftwonum.java
@@ -0,0 +1,23 @@
+// pseudocode for sum of two numbers
+/*
+Start
+input numbers a & b
+calculate sum = a + b
+print sum
+Exit
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Sumoftwonum{
+
+    public static void main(String arg[]){
+        int a=10,b=20,sum;
+
+        sum = a + b;
+
+        System.out.println("sum of a & b is:"+ sum);
+}
+}
